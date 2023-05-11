237d0e10950b5f0e266e7c1600abd06c39a39421
penhaMa <130068243+penhaMa@users.noreply.github.com>
2023-05-11 20:42:23 -0300

    Terceiro commit

--- calculadoraCentifica/Calculo.cs
+++ calculadoraCentifica/Calculo.cs
@@ -8,7 +8,47 @@ namespace calculadoraCentifica
 {
      class Calculo
     {
+        //Método Soma
+        public static int Soma(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            return resultado;
+        }//Fim do método Soma
 
+        //Método Subtração
+        public static int Subtracao(int num1, int num2)
+        {
+            int resultado = num1 - num2;
+            return resultado;
+        }//Fim do método Subtração
+
+        //Método Divisão
+        public static int Divisao(int num1, int num2)
+        {
+            int resultado = num1 / num2;
+            return resultado;
+        }//Fim do método Divisão
+
+        //Método Multiplicação
+        public static int Multiplicacao(int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            return resultado;
+        }//Fim do método Multiplicação
+
+        //Método Potência
+        public static int Potencia(int num1, int num2)
+        {
+            int resultado = num1 ^ num2;
+            return resultado;
+        }//Fim do método Potência
+
+        //Método Média
+        public static int Media(int num1, int num2)
+        {
+            int resultado = (num1 + num2) / 2;
+            return resultado;
+        }//Fim do método Média
 
     }
 }
